48072f9e854700fa704ed981fef8686c9a6fd73b
Yassine <yassin23raki@gmail.com>
2020-10-02 10:56:52 +0100

    movies information

--- index.js
+++ index.js
@@ -9,21 +9,104 @@ const fetchData = async (searchTerm) => {
     return response.data.Search;
 }
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+    <label><b>Search For A Movie</b></label>
+    <input class='input'/>
+    <div class="dropdown">
+        <div class="dropdown-menu">
+          <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
+
+
+const dropdown = document.querySelector('.dropdown');
 const input = document.querySelector('input');
+const resultsWrapper = document.querySelector('.results');
 
 const onInput = async event => {
     const movies = await fetchData(event.target.value);
 
+    if (!movies.length) { dropdown.classList.remove('is-active'); return; }
+
+    resultsWrapper.innerHTML = '';
+
+    dropdown.classList.add('is-active');
+        
     for (let movie of movies) {
-        const div = document.createElement('div');
+        const option = document.createElement('a');
+
+        const imgSRC = movie.Poster === 'N/A' ? '' : movie.Poster;
         
-        div.innerHTML = `
-            <img src='${movie.Poster}' /> 
-            <h1>${movie.Title}</h1>
+        option.classList.add('dropdown-item')
+
+        option.innerHTML = `
+            <img src='${imgSRC}'/> 
+            ${movie.Title}
         `;
+
+        option.addEventListener('click', () => {
+            dropdown.classList.remove('is-active');
+            input.value = movie.Title;
+            onMovieSelect(movie);
+        })
         
-        document.querySelector('#target').appendChild(div)
+        resultsWrapper.appendChild(option);
     }
 };
 
 input.addEventListener('input', debounce(onInput, 1000));
+
+document.addEventListener('click', event => {
+    if (!root.contains(event.target)) { dropdown.classList.remove('is-active'); }
+})
+
+const onMovieSelect = async (movie) => {
+    const respone = await axios.get('http://www.omdbapi.com/', {
+        params : {
+            apikey : '22162db0',
+            i : movie.imdbID
+        }
+    })
+    document.querySelector('#summary').innerHTML = movieTemplate(respone.data);
+}
+
+const movieTemplate = (movieDetail) => {
+    return `
+        <article class='media'>
+            <figure class='media-left'>
+                <p class='image'>
+                    <img src='${movieDetail.Poster}'/>
+                </p>
+            </figure>
+            <div class=''media-content'>
+                <div class='content'>
+                    <h1>${movieDetail.Title}</h1>
+                    <h4>${movieDetail.Genre}</h4>
+                    <p>${movieDetail.Plot}</p>
+                </div>
+            </div>
+        </article>
+        <article class='notification is-primary'>
+            <p class='title'>${movieDetail.Awards}</p>
+            <p class='subtitle'>Awards</p>
+        </article>
+        <article class='notification is-primary'>
+            <p class='title'>${movieDetail.BoxOffice}</p>
+            <p class='subtitle'>Box Office</p>
+        </article>
+        <article class='notification is-primary'>
+            <p class='title'>${movieDetail.Metascore}</p>
+            <p class='subtitle'>MetaScore</p>
+        </article>
+        <article class='notification is-primary'>
+            <p class='title'>${movieDetail.imdbRating}</p>
+            <p class='subtitle'>IMDB Rating</p>
+        </article>
+        <article class='notification is-primary'>
+            <p class='title'>${movieDetail.imdbVotes}</p>
+            <p class='subtitle'>IMDB Votes</p>
+        </article>
+    `;
+}
